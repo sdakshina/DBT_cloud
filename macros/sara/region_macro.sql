6ab15174fcbdf7319a4a6b07242d0ee81823d2d5
saran b <saravanan199773@gmail.com>
2026-05-07 17:11:13 +0000

    commit

--- macros/sara/region_macro.sql
+++ macros/sara/region_macro.sql
@@ -4,13 +4,13 @@
     {% endset %}
     {% set result = run_query(code) %}
     {% if execute %}
-        {% set region_lst = result.column[0].values()%}
+        {% set region_lst = result.columns[0].values()%}
     {% else %}
         {% set region_lst = [] %}
     {% endif %}
     select 
     {% for region in region_lst %}
-        sum( case when region = '{{region}}'  then amount else 0 end ) as col_{{region}}
+        sum( case when region = '{{region}}'  then amount else 0 end ) as "col_{{region}}"
         {% if not loop.last %} , {% endif %} 
     {% endfor %}
      from {{source(schema,table_name)}}
